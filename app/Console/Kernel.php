84141e4689e6a5cdbc9e11cd8137457e130e5edd
Idegart <degtyarev_artem@mail.ru>
2019-07-23 12:07:22 +0300

    Add queue to cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('queue:work --daemon --tries=1 --sleep=0')
+            ->everyMinute()
+            ->withoutOverlapping();
     }
 
     /**
