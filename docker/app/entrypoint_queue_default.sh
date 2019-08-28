59423932ed7d6946011a202c447aa281ca9bfad7
Idegart <degtyarev_artem@mail.ru>
2019-08-28 14:38:24 +0300

    Set tries to 1

--- docker/app/entrypoint_queue_default.sh
+++ docker/app/entrypoint_queue_default.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./docker/app/entrypoint.sh
-sudo -E -u www-data php artisan queue:work --daemon --queue=default
+sudo -E -u www-data php artisan queue:work --daemon --queue=default --tries=1
 
